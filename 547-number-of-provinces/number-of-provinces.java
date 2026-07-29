f4cd90223004f02aa1167b3f1271245bca00a969
GauriKansal6533 <gaurikansal6533@gmail.com>
2026-07-29 12:08:00 +0530

    Time: 3 ms (26.44%) | Memory: 49.1 MB (17.80%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,37 +1,40 @@
 class Solution {
-    public static void dfs(int node,boolean[] visited,ArrayList<ArrayList<Integer>> adjList){
-        visited[node]=true;
-        for(int neighbour:adjList.get(node)){
-            if(visited[neighbour]==false){
-                dfs(neighbour,visited,adjList);
+    public static void dfs(int V,ArrayList<ArrayList<Integer>> adjList,boolean visited[]){
+        visited[V]=true;
+        for(int neighbour:adjList.get(V)){
+            if( visited[neighbour]==false){
+                dfs(neighbour,adjList,visited);
             }
         }
+
     }
     public int findCircleNum(int[][] isConnected) {
-        int node=isConnected.length;
+        int V=isConnected.length;
+        //convert adjmatrix to adjlist
         ArrayList<ArrayList<Integer>> adjList=new ArrayList<>();
-        for(int i=0; i<node;i++){
+        //initialize
+        for(int i=0; i<V; i++){
             adjList.add(new ArrayList<>());
-
+            
         }
-        //convert matrix to list
-        for(int i=0; i<node;i++){
-            for(int j=0; j<node;j++){
+        //convert
+        for(int i=0; i<V; i++){
+            for(int j=0; j<V; j++){
                 if(isConnected[i][j]==1 && i!=j){
-                    adjList.get(i).add(j);
-                    adjList.get(j).add(i);
-                }
+            adjList.get(i).add(j);
+            adjList.get(j).add(i);
+        }
             }
         }
-        boolean visited[]=new boolean[node];
+        boolean visited[]=new boolean[V];
         int count=0;
-        for(int i=0; i<node; i++ ){
-            if(!visited[i]){
-                count++;
-                dfs(i,visited,adjList);
+        for(int i=0; i<V; i++){
+            if(visited[i]==false){
+                visited[i]=true;
+                dfs(i,adjList,visited);
+                count ++;
             }
         }
         return count;
-        
     }
 }
